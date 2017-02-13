6681264cfe223ca8f7ad78f618c92a55e42e0806
Justin Jordan <jordan.justinb@gmail.com>
2017-02-13 08:04:18 -0600

    started training program2/13/17  jbj

---
+++ createTrainingFile.py
@@ -0,0 +1,14 @@
+"""******************************************************************
+File Name:createTrainingFile.py
+Authors: Justin Jordan, David Ikemba, Thuong Nguyen
+Date:
+Project: Machine Vision Burr Detection System
+Sponsor: Hunt and Hunt Ltd.
+Faculty Advisor: Dr. Fred Chen
+Instructor: Dr. Compeau
+University: Texas State University Ingram School of Engineering
+Description: The purpose of this program is to create a .vec
+   file to be used for the purposes of training a Haar cascade
+   capable of detecting a burr-free threading keyway intersection.
+******************************************************************"""
+
